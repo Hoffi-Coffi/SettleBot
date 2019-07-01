f79041847c485204a0541a9bc9c419e087f31547
Hoffi Coffi <timesplitter08+hof@gmail.com>
2019-07-01 17:56:31 +0100

    fix tests??

--- test/setupJest.ts
+++ test/setupJest.ts
@@ -1,11 +1,1 @@
 import "reflect-metadata";
-import * as fs from 'fs';
-
-var badWordsModel = {
-    delete: [],
-    mute: []
-};
-
-fs.writeFile('./src/badwords.json', JSON.stringify(badWordsModel), (err) => {
-    if (err) throw err;
-});
