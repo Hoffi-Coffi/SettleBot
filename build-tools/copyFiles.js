3361953faa0e7bbf362dab6ef2883be6debf4992
Hoffi Coffi <timesplitter08+hof@gmail.com>
2019-11-17 01:10:14 +0000

    overhaul stats command to use image

--- build-tools/copyFiles.js
+++ build-tools/copyFiles.js
@@ -2,13 +2,22 @@ var fs = require('fs');
 
 var filesToCopy = ["badwords.json", "config.json", 
     "rsns.json", "stats.json", "events.json", "sotw.json", "leaderboards.json"];
+var imagesToCopy = ["statsbg.png", "runescape_uf.ttf"];
 
 var outDir = "./dist/bot/";
 var cmdArgs = process.argv.slice(2);
 if (cmdArgs[0] !== '--prod') {
     outDir = "./local-run/bot/";
+
     filesToCopy.forEach((val) => {
         fs.copyFile(`./src/${val}`, `./local-run/${val}`, (err) => {
+            if (err) throw err;
+            console.log(`Copied file "${val}" successfully`);
+        });
+    });
+
+    imagesToCopy.forEach((val) => {
+        fs.copyFile(`./src/img/${val}`, `./local-run/${val}`, (err) => {
             if (err) throw err;
             console.log(`Copied file "${val}" successfully`);
         });
